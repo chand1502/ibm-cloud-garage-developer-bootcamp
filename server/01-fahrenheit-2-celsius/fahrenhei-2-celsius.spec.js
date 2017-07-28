074df49da365c5f8bdbf0600a2fa9e93e2e2f7e1
Chandana Mulpuri <chandanamulpuri@Chandanas-MacBook-Air.local>
2017-07-28 14:11:32 -0500

    make second test pass

--- server/01-fahrenheit-2-celsius/fahrenhei-2-celsius.spec.js
+++ server/01-fahrenheit-2-celsius/fahrenhei-2-celsius.spec.js
@@ -3,15 +3,16 @@ describe.only('the fahrenheit2celsius canary spec', () => {
     true.should.be.true();
   });
 
-  function farenheit2celsius() {
-    return 32;
+  function farenheit2celsius(temperature) {
+    return (temperature - 32) * 5 / 9;
   }
 
   describe('fahrenheit2celsius should', () => {
     it('give 0°C for 32°F', () => {
-      farenheit2celsius(0).should.equal(32);
+      farenheit2celsius(32).should.equal(0);
     });
-
-    it('give 10°C for 50°F');
+    it('give 10°C for 50°F', () => {
+      farenheit2celsius(50).should.equal(10);
+    });
   });
 });
