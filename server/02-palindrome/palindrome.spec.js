db531184b6a75d1b89d3fbac14221e2b6f35288b
Chandana Mulpuri <chandanamulpuri@Chandanas-MacBook-Air.local>
2017-07-28 15:45:58 -0500

    palindrome change 2

--- server/02-palindrome/palindrome.spec.js
+++ server/02-palindrome/palindrome.spec.js
@@ -3,8 +3,9 @@ describe.only('the palindrome canary spec', () => {
     true.should.be.true();
   });
 
-  function ispalindrome(word) {
-    return word === word.split('').reverse().join('');
+  function ispalindrome(phrase) {
+    if (phrase.trim().length === 0) return false;
+    return phrase.split('').reverse().join('') === phrase;
   }
 
   describe('a palindrome', () => {
@@ -14,9 +15,17 @@ describe.only('the palindrome canary spec', () => {
     it('is not racecars', () => {
       ispalindrome('racecars').should.be.false();
     });
-    it('is not race car');
-    it('"      " is not a palindrome');
-    it('"" is not a palindrome');
-    it('is "mom dad mom"');
+    it('is not race car', () => {
+      ispalindrome('race car').should.be.false();
+    });
+    it('"      " is not a palindrome', () => {
+      ispalindrome('      ').should.be.false();
+    });
+    it('"" is not a palindrome', () => {
+      ispalindrome('').should.be.false();
+    });
+    it('is "mom dad mom"', () => {
+      ispalindrome('mom dad mom').should.be.true();
+    });
   });
 });
