38ea3589d20abc7b266d215e0b1789da895aec3d
Chandana Mulpuri <chandanamulpuri@chandanas-air.dfw.ibm.com>
2017-07-31 10:26:05 -0500

    change stack test 3

--- server/03-stack/stack.spec.js
+++ server/03-stack/stack.spec.js
@@ -1,15 +1,29 @@
-let stack = {
-  isEmpty: () => true
+const makeStack = () => {
+  let empty = true;
+
+  return {
+    isEmpty: () => empty,
+    size: () => 0,
+    push: () => {empty = false;}
+ };
 };
+
+let stack = makeStack();
 
 describe.only('the stack', () => {
   it('start empty', () => {
     stack.isEmpty().should.be.true();
   });
-  it('starts with stack size 0', () => {
 
+  it('starts with stack size 0', () => {
+    stack.size().should.equal(0);
   });
-  it('is not empty when pushed');
+
+  it('is not empty when pushed', () => {
+    stack.push();
+    stack.isEmpty().should.be.false();
+  });
+
   it('leaves stack size 1 when pushed');
   it('leaves stack empty when pushed and popped');
   it('leaves stack size 0 when pushed and popped');
