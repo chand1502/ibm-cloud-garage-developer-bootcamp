df708dd1d5eece87f4f82f6adf849116d0f691ba
Chandana Mulpuri <chandanamulpuri@Chandanas-MacBook-Air.local>
2017-08-03 16:41:09 -0500

    --no-verify

--- src/main/java/com/ibm/cloud/JavaRefactoringTrial.java
+++ src/main/java/com/ibm/cloud/JavaRefactoringTrial.java
@@ -7,16 +7,16 @@ import java.util.List;
 public class JavaRefactoringTrial {
 	public static void main(String[] args) {
 		List<Person> personList = new ArrayList<Person>();
-		
+
 		try {
 			personList.addAll(PersonLoader.loadPersonRecordsFromSpaceFile());
 			personList.addAll(PersonLoader.loadPersonRecordsFromPipeFile());
 			personList.addAll(PersonLoader.loadPersonRecordsFromCommaFile());
-			
-			PersonOutputGenerator.generateOutput1(personList);
-			PersonOutputGenerator.generateOutput2(personList);
-			PersonOutputGenerator.generateOutput3(personList);
-			
+
+			PersonOutputGenerator.generateOutput1(personList, PersonOutputGenerator.genderThenLastNameAsc);
+			PersonOutputGenerator.generateOutput2(personList, dobThenLastNameAsc);
+			PersonOutputGenerator.generateOutput3(personList, LastNameDesc);
+
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
--- src/main/java/com/ibm/cloud/PersonOutputGenerator.java
+++ src/main/java/com/ibm/cloud/PersonOutputGenerator.java
@@ -1,60 +1,62 @@
 package com.ibm.cloud;
 
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
-public class PersonOutputGenerator {
-	public static void generateOutput1(List<Person> personList) {		
-		Collections.sort(personList, new Comparator<Person>() {
-			// sort by Gender, then by LastName in ascending order
-			public int compare(Person p1, Person p2) {
-				if (!p1.getGender().equals(p2.getGender())) {
-					return p1.getGender().compareTo(p2.getGender());
-				}
-				
-				return p1.getLastName().compareTo(p2.getLastName());
-			}
-		});
-		
-		System.out.println("Output 1:\n");
+class PersonOutputGenerator {
+  static void generateOutput1(List<Person> personList, String title, Comparator<Person> personComparator) {
+    personList.sort(personComparator);
+    printPersons(personList, title);
+  }
 
-		for (Person person : personList) {
-			System.out.println(person);
-		} 
-	}
-	
-	public static void generateOutput2(List<Person> personList) {		
-		Collections.sort(personList, new Comparator<Person>() {
-			// sort by DateOfBirth in ascending order, then by LastName in ascending order
-			public int compare(Person p1, Person p2) {
-				if (p1.getDateOfBirth().compareTo(p2.getDateOfBirth()) != 0) {
-					return p1.getDateOfBirth().compareTo(p2.getDateOfBirth());
-				}
-				
-				return p1.getLastName().compareTo(p2.getLastName());
-			}
-		});
-		
-		System.out.println("\nOutput 2:\n");
-
-		for (Person person : personList) {
-			System.out.println(person);
-		} 		
-	}
-	
-	public static void generateOutput3(List<Person> personList) {		
-		Collections.sort(personList, new Comparator<Person>() {
-			// sort by LastName in descending order
-			public int compare(Person p1, Person p2) {
-				return p2.getLastName().compareTo(p1.getLastName());
-			}
-		});
-		
-		System.out.println("\nOutput 3:\n");
-
-		for (Person person : personList) {
-			System.out.println(person);
-		} 		
-	}
 }
+
+  private static List<Person> printPersons(List<Person> personList) {
+    return personList;
+  }
+
+  static void generateOutput2(List<Person> personList, Comparator<? super Person> personComparator) {
+    printPersons(personList).sort(personComparator);
+    System.out.println("\nOutput 2:\n");
+
+    for(Person person : printPersons(personList)) {
+      System.out.println(person);
+    }
+  }
+
+  static void generateOutput3(List<Person> personList, Comparator<? super Person> personComparator) {
+    personList.sort(personComparator);
+    System.out.println("\nOutput 3:\n");
+
+    for(Person person : personList) {
+      System.out.println(person);
+    }
+  }
+}
+  Comparator<Person> genderThenLastNameAsc=new Comparator<Person>(){
+// sort by Gender, then by LastName in ascending order
+public int compare(Person p1,Person p2){
+if(!p1.getGender().equals(p2.getGender())){
+return p1.getGender().compareTo(p2.getGender());
+}
+
+return p1.getLastName().compareTo(p2.getLastName());
+}
+};
+private Comparator<Person> dobThenLastNameAsc=new Comparator<Person>(){
+// sort by DateOfBirth in ascending order, then by LastName in ascending order
+public int compare(Person p1,Person p2){
+if(p1.getDateOfBirth().compareTo(p2.getDateOfBirth())!=0){
+return p1.getDateOfBirth().compareTo(p2.getDateOfBirth());
+}
+
+return p1.getLastName().compareTo(p2.getLastName());
+}
+};
+Comparator<Person> LastNameDesc=new Comparator<Person>(){
+// sort by LastName in descending order
+public int compare(Person p1,Person p2){
+return p2.getLastName().compareTo(p1.getLastName());
+}
+};
+
